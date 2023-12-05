b7e888ecb8c47e5ec8afd490f7c45d87afa0fe41
Namanqw1 <nasuchacha1708@gmail.com>
2023-12-05 15:04:17 +0530

    post fix exp calculator

--- postfixslove.py
+++ postfixslove.py
@@ -31,7 +31,7 @@ class stack:
             print("Error : Your stack size if full.")
     
     def pop(self): # funcation to pop the item from the stack
-        if self.stEmpty == False:
+        if self.stEmpty() == False:
             pop_item = self.stack[self.top]
             print(str(pop_item) + " item poped from stack from index " + str(self.top))
             self.top -= 1
@@ -44,6 +44,45 @@ class stack:
         return stack_size
 
     def peek(self):
-        if not self.is_empty():
+        if not self.stEmpty:
             return self.items[self.top]
         return None
+
+stack = stack(10)
+
+def evaluate_postfix_with_stack(expression):
+    operators = set(['+', '-', '*', '/', '^'])
+
+    for char in expression:
+        if char.isdigit():
+            stack.push(int(char))
+        elif char in operators:
+            operand2 = stack.pop()
+            operand1 = stack.pop()
+            if operand1 is None or operand2 is None:
+                return "Invalid Expression"
+
+            if char == '+':
+                stack.push(operand1 + operand2)
+            elif char == '-':
+                stack.push(operand1 - operand2)
+            elif char == '*':
+                stack.push(operand1 * operand2)
+            elif char == '/':
+                if operand2 == 0:
+                    return "Division by zero error"
+                stack.push(operand1 / operand2)
+            elif char == '^':
+                stack.push(operand1 ** operand2)
+
+    result = stack.pop()
+    if stack.stEmpty() and result is not None:
+        return result
+    else:
+        return "Invalid Expression"
+
+
+# Example usage:
+postfix_expression = "42^3*5-"
+result = evaluate_postfix_with_stack(postfix_expression)
+print("Result:", result)
